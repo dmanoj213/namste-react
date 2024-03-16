9b9d3a83b3f6f642b36235431c611e690ffeb968
manojvadhi <dmanoj213@gmail.com>
2024-03-16 12:51:01 +0530

    proxy 1234 commit

--- .proxyrc.js
+++ .proxyrc.js
@@ -3,10 +3,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:8000/",
+      target: "http://localhost:1234/",
       pathRewrite: {
         "^/api": "",
       },
     })
   );
 };
+
